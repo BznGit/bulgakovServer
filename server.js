f8578765457faa24b5cdd8895c81dcc38a5397f2
BznGit <beznyakov@gmail.com>
2024-03-19 17:22:38 +0300

    port 9000

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 const server = require('http').Server(app);
 const bodyParser = require('body-parser');
 
-server.listen(9000, ()=>console.log("EAscan server started on port: 3000"));
+server.listen(9000, ()=>console.log("EAscan server started on port: 9000"));
 app.use(express.static('./dist')); 
 
